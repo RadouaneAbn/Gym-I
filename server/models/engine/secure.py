c17167b09ede436dce4b6d177f03cac113580224
RadouaneAbn <radouaneabn@gmail.com>
2024-05-19 17:37:55 +0100

    last changes

--- server/models/engine/secure.py
+++ server/models/engine/secure.py
@@ -1,8 +1,6 @@
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="client/user/login")
 
 
 def verify_password(plain_password, hashed_password):
